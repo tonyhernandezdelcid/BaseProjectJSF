1e65d83ef21734a113feca87e5d6f0073c1be95b
tonyhernandezdelcid <tonyhernandezdelcid@gmail.com>
2023-05-30 12:31:04 -0600

    Creación de nuevos beans - menu

---
+++ src/java/com/ah/baseprojectjsf/beans/BeanMenu.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ah.baseprojectjsf.beans;
+
+/**
+ *
+ * @author AHERNANDEZ
+ */
+public class BeanMenu {
+
+    /**
+     * Creates a new instance of BeanMenu
+     */
+    public BeanMenu() {
+    }
+    
+}
